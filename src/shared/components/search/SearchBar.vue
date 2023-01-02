d990ecaf7f515e5497e2e6afb98e25483b23f24f
Federico Baldini <federico.baldini7@gmail.com>
2023-01-02 17:54:19 +0100

    Update SearchBar.vue
    Added clear input text action.

--- src/shared/components/search/SearchBar.vue
+++ src/shared/components/search/SearchBar.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
+import { type Ref, ref } from "vue";
+
 const emit = defineEmits<{
   (event: "onChangeInputText", value: string): void;
 }>();
+const inputText: Ref<string> = ref("");
+
+const inputHandler = (text: string) => {
+  inputText.value = text;
+  emit("onChangeInputText", text);
+};
+
+const clearInputHandler = () => {
+  inputText.value = "";
+  emit("onChangeInputText", "");
+};
 </script>
 
 <template>
   <div class="searchbar">
-    <input
-      type="text"
-      placeholder="What are you looking for?"
-      class="searchbar__input"
-      @input="(event) => emit('onChangeInputText', (event.target as HTMLInputElement).value)"
-    />
+    <div class="searchbar__input-container">
+      <input
+        type="text"
+        placeholder="What are you looking for?"
+        class="searchbar__input"
+        :value="inputText"
+        @input="(event) => inputHandler((event.target as HTMLInputElement).value)"
+      />
+      <span class="searchbar__clear-input" @click="clearInputHandler">X</span>
+    </div>
   </div>
 </template>
 
@@ -20,12 +37,18 @@ const emit = defineEmits<{
   padding: 2rem 1rem 1rem 1rem;
   width: 100%;
   text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
+.searchbar__input-container {
+  position: relative;
+  max-width: 40rem;
+  width: 100%;
+}
 .searchbar__input {
   background: #3b3b3b;
   color: #bcbcbc;
-  max-width: 40rem;
   width: 100%;
   height: 3rem;
   text-decoration: none;
@@ -42,10 +65,18 @@ const emit = defineEmits<{
   -o-appearance: none;
   appearance: none;
 }
-
 .searchbar__input:active,
 .searchbar__input:focus {
   outline: none;
   border-color: #82e0aa;
+}
+.searchbar__clear-input {
+  font-size: 20px;
+  color: #bcbcbc;
+  position: absolute;
+  top: 1rem;
+  right: 2rem;
+  font-weight: bold;
+  cursor: pointer;
 }
 </style>
