fe96c2663ef49822ccae67fd9ae9aa3083bda508
Federico Baldini <federico.baldini7@gmail.com>
2023-01-02 16:34:46 +0100

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/book-collection-site/",
+  base: "/book-collection/",
   plugins: [vue()],
   resolve: {
     alias: {
